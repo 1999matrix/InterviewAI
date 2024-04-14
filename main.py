76e4a36e022365ec6f5471b200b0dfe914937ba7
Bhupendra Rajput <bhupendra360i@gmail.com>
2024-04-14 18:45:35 +0530

    modify main.py file

--- main.py
+++ main.py
@@ -1,60 +1,7 @@
-# from flask import Flask, jsonify
-# import schedule
-# import threading
-# import time
-# import os
-# from src.component.voice_decryption import WavToTextConverter
-# from src.component.start import QuestionFetcher
-# from datetime import datetime, timedelta
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-
-
-# app = Flask(__name__)
-
-
-# from flask import Flask, request
-# app = Flask(__name__)
-
-# @app.route("/api/v1/start_test", methods=["POST"])
-# def start_test():
-#     try:
-#         # Extract parameters from the POST request
-#         username = request.form.get("username")
-#         topic = request.form.get("topic")
-#         level = request.form.get("level")
-
-#         # Create an instance of QuestionFetcher with the extracted parameters
-#         question_id_fetcher_instance = QuestionFetcher(username, topic, level)
-#         question_id = question_id_fetcher_instance.fetch_questions()
-
-#         return str(question_id)
-#     except Exception as e:
-#         return str(e), 500
-
-
-
-
-
-# @app.route("/api/v1/decode", methods=["POST"])
-# def decode_audio():
-#     # Call your main function here
-#     try:
-#         WavToTextConverter_instance = WavToTextConverter()
-#         wav_file_path = os.getenv("recording_path")
-#         result = WavToTextConverter_instance.convert(wav_file_path)
-#         return jsonify({"result": result})
-#     except Exception as e:
-#         return str(e), 500
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-
-
+import schedule
+import threading
+import time
+from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
 import os
 from src.component.voice_decryption import WavToTextConverter
@@ -83,17 +30,6 @@ def start_test():
 
 
 
-# @app.route("/api/v1/voice_decode", methods=["POST"])
-# def decode_audio():
-#     # Call your main function here
-#     try:
-#         WavToTextConverter_instance = WavToTextConverter()
-#         wav_file_path = os.getenv("recording_path")
-#         result = WavToTextConverter_instance.convert(wav_file_path)
-#         return jsonify({"result": result})
-#     except Exception as e:
-#         return str(e), 500
-
 
 import os
 from flask import request, jsonify
@@ -118,15 +54,10 @@ def decode_audio():
         # Get file path from the request
         file_path = request.form.get("file_path")
 
-        # Save the file to the specified file path
-        # file.save(file_path)
 
-        # Call the WavToTextConverter with the file path
         WavToTextConverter_instance = WavToTextConverter()
         result = WavToTextConverter_instance.convert(username,file_path)
 
-        # Delete the temporary file
-        # os.remove(file_path)
 
         return jsonify({"result": result})
     
