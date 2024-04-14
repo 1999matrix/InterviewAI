152ee5206e87f1052d2a9934f3cdd731cdb00056
Bhupendra Rajput <bhupendra360i@gmail.com>
2024-04-14 18:42:59 +0530

    added one connection made to database fucntion

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 from dotenv import load_dotenv
 import os
 import sys
+import mysql.connector
 
 
 def convert_wav_to_text(wav_file):
@@ -16,3 +17,19 @@ def convert_wav_to_text(wav_file):
         
         except sr.UnknownValueError:
             print("could not understand the audio")
+
+
+def connect_to_user_db_and_table2():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Admin@12345",
+            database="db_77"
+        )
+        if connection.is_connected():
+            print("Connected to MySQL Server")
+        return connection
+    except mysql.connector.Error as error:
+        print("Error:", error)
+        return None
