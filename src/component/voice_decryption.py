40fc986eb4a34d33bfd3a2c5924d421cb3bbf683
Bhupendra Rajput <bhupendra360i@gmail.com>
2024-04-14 18:39:34 +0530

    modiify voice_decryption.py file

--- src/component/voice_decryption.py
+++ src/component/voice_decryption.py
@@ -3,20 +3,50 @@ from dotenv import load_dotenv
 import os
 import sys
 from src.utils import convert_wav_to_text 
+from src.utils import connect_to_user_db_and_table2 
 
 load_dotenv()
 
-# wav_file_path = os.getenv("recording_path")
-# text = convert_wav_to_text(wav_file_path)
-# print("Text:", text)
 
 class WavToTextConverter:
-    def convert(self, wav_file_path):
+    
+    def convert(self, username, wav_file_path):
         # Convert the WAV file to text
         text = convert_wav_to_text(wav_file_path)
+        
+        # Update the database with the converted text
+        db_connection = connect_to_user_db_and_table2()
+        if db_connection:
+            cursor = db_connection.cursor()
+            print("going to send data in table2")
+            # Construct the SQL query
+            sql_query = f"UPDATE table2 SET response = CONCAT_WS(',', IF(response='', NULL, response), %s) WHERE username = %s"
+            # Note: CONCAT_WS should be used to add a comma between existing text and new text
+            
+            print("query is ready")
+            try:
+                # Execute the SQL query
+                cursor.execute(sql_query, (text, username))
+                print("cursor is ready")
+                # Commit the changes
+                db_connection.commit()
+                print("send data to table2 successfully")
+            except Exception as e:
+                print("Error occurred while executing SQL query:", e)
+                # Rollback the changes
+                db_connection.rollback()
+                return "Failed to store response"
+            finally:
+                # Close the cursor and the database connection
+                cursor.close()
+                db_connection.close()
+                print("connection closed")
+            
+            return "Response stored successfully"
+        else:
+            return "Failed to connect to the database"
 
-        # Print the text
-        print("Text:", text)
+
 
 
 if __name__ == "__main__":
