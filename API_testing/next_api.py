7b4c0eb63e4384cbc34b60c0cc6062cad0e0abd9
Bhupendra Rajput <bhupendra360i@gmail.com>
2024-09-27 22:51:27 +0530

    question return instead of id in next api response

--- API_testing/next_api.py
+++ API_testing/next_api.py
@@ -6,7 +6,7 @@ url = 'http://localhost:7777/api/v1/get_next_question_id'
 # Define the parameters you want to send
 params = {'username': 'example_user',
           'topic':'Python_db',
-          'level':'level_low',
+          'level':'low',
           'text':'python is a high level interpreter language'}
 
 # Send a GET request to the API endpoint
--- src/component/next.py
+++ src/component/next.py
@@ -3,7 +3,7 @@ from src.utils import connect_to_user_db
 from src.component.result import ResponseFetcher
 import threading 
 from src.component.text_to_db import TextAppender
-
+from src.utils import fetch_question
 
 class QuestionManager(TextAppender):
     def __init__(self):
@@ -25,13 +25,23 @@ class QuestionManager(TextAppender):
 
     def get_next_question_id(self, username, topic, level):  
         connection = None
+
+        if level == "low":
+            next_id_question_table = "level_low"
+        elif level == "medium":
+            next_id_question_table = "level_medium"
+        elif level == "advance":
+            next_id_question_table = "level_high"
+        else:
+            print("Invalid topic or level")
+
         try:
             # Connect to the user database
             connection = connect_to_user_db()
             cursor = connection.cursor()
             
             # Start the analysis trigger in a separate thread
-            thread = threading.Thread(target=self.trigger_analysis, args=(username, topic, level))
+            thread = threading.Thread(target=self.trigger_analysis, args=(username, topic, next_id_question_table))
             thread.start()
             
             # Check if the username exists in the table
@@ -43,7 +53,9 @@ class QuestionManager(TextAppender):
                 q_id_list = q_ids.split(',')  # Split the comma-separated q_ids into a list
                 if len(q_id_list) > response_count:
                     next_q_id = q_id_list[response_count]  # Get the next q_id based on response_count
-                    return next_q_id
+
+                    question = fetch_question(topic,next_id_question_table,next_q_id)
+                    return question
                 else:
                     return None  # No more questions left for this user
             else:
